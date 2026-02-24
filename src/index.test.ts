f922df0f7ebc91d099b792727d6985185826f8eb
Jeroen de Bruijn <vv9239@gmail.com>
2026-02-24 07:29:43 +0100

    test: remove v19 tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,6 +1,4 @@
 import test from 'ava';
-import load19x from '@commitlint/load-19.x';
-import lint19x from '@commitlint/lint-19.x';
 import load20x from '@commitlint/load-20.x';
 import lint20x from '@commitlint/lint-20.x';
 import {type LoadOptions} from '@commitlint/types';
@@ -9,7 +7,6 @@ import config from './index.js';
 
 /* eslint-disable @typescript-eslint/naming-convention */
 const versions = {
-  '19.x': {load: load19x, lint: lint19x},
   '20.x': {load: load20x, lint: lint20x},
 } as const;
 /* eslint-enable @typescript-eslint/naming-convention */
@@ -25,23 +22,6 @@ test('uses the "function-rules" plugin', (t) => {
 test('creates the "function-rules/header-max-length" rule', (t) => {
   const rule = config.rules?.['function-rules/header-max-length'];
   t.deepEqual(rule, [2, 'always', headerMaxLength]);
-});
-
-/**
- * The type of the `RulesConfig` have changed in [commitlint v20.3.1](
- * https://github.com/conventional-changelog/commitlint/releases/tag/v20.3.1),
- * and are no longer compatible with those from v19. Specifically the types of
- * the `scope-case` and `scope-enum` rules have been changed to also accept a
- * rule object. The `config` should therefore not use these rules, specifically
- * not the object version, to ensure backwards compatibility with v19. This
- * tests validates that is the case and warrants the use of `as unknown as any`
- * in some of the tests below.
- */
-test('does not use the "scope-case" and "scope-enum" rules', (t) => {
-  t.is(config.rules?.['scope-case'], undefined);
-  t.is(config.rules?.['function-rules/scope-case'], undefined);
-  t.is(config.rules?.['scope-enum'], undefined);
-  t.is(config.rules?.['function-rules/scope-enum'], undefined);
 });
 
 const disables = test.macro<[string]>({
@@ -62,41 +42,28 @@ test(disables, 'header-max-length');
 const loadOptions: LoadOptions = {file: 'package.json'} as const;
 const loadConfig = test.macro<[keyof typeof versions]>({
   async exec(t, version) {
-    await t.notThrowsAsync(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      versions[version].load(config as unknown as any, loadOptions),
-    );
+    await t.notThrowsAsync(versions[version].load(config, loadOptions));
   },
   title: (_, version) =>
     `@commitlint/load@${version} can load the configuration`,
 });
-
-test(loadConfig, '19.x');
 test(loadConfig, '20.x');
 
 const lintUsingConfiguration = test.macro<[keyof typeof versions]>({
   async exec(t, version) {
-    const qualifiedConfig = await versions[version].load(
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      config as unknown as any,
-      loadOptions,
-    );
+    const qualifiedConfig = await versions[version].load(config, loadOptions);
     const input = [
       'chore(deps): we are told to remember the idea, not the man. Because a',
       'man can fail. He can be caught, he can be killed and forgotten. But 400',
       'years later, an idea can still change the world - A. Moore',
     ].join(' ');
-    const outcome = await versions[version].lint(
-      input,
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-      qualifiedConfig.rules as unknown as any,
-      {plugins: qualifiedConfig.plugins},
-    );
+    const outcome = await versions[version].lint(input, qualifiedConfig.rules, {
+      plugins: qualifiedConfig.plugins,
+    });
 
     t.deepEqual(outcome, {input, valid: true, errors: [], warnings: []});
   },
   title: (_, version) =>
     `@commitlint/lint@${version} can use the configuration`,
 });
-test(lintUsingConfiguration, '19.x');
 test(lintUsingConfiguration, '20.x');
